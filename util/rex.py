d2c0e5d63d9d2344bff52c35811b6486fbfcd477
zyujs <zyujs@msn.com>
2021-06-07 08:17:44 +0800

    fix #2

--- util/rex.py
+++ util/rex.py
@@ -7,7 +7,11 @@ def rex_replace(rex_pattern, func, new_rex_pattern = None):
         new_rex = re.compile(new_rex_pattern)
     for k in list(rex.allrex.keys()):
         if rex_pattern in k.pattern:
-            rex.allrex[k].func = func
+            if isinstance(rex.allrex[k], list):
+                for i in range(0, len(rex.allrex[k])):
+                    rex.allrex[k][i].func = func
+            else:
+                rex.allrex[k].func = func
             if new_rex:
                 rex.allrex[new_rex] = rex.allrex.pop(k)
             return True
